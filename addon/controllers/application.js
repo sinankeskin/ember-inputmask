3a6aa5bcd1e9e8756dfe4f4cef57b72014e2c3eb
Sinan Keskin <sinan@kesk.in>
2022-02-13 13:54:18 +0300

    Remove controller

--- addon/controllers/application.js
+++
@@ -1,9 +0,0 @@
-import { action } from '@ember/object';
-import Controller from '@ember/controller';
-
-export default class ApplicationController extends Controller {
-  @action
-  onUpdate(value) {
-    console.log('value', value);
-  }
-}
