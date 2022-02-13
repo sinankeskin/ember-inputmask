68444aa9794e502f1f4b6b240719c89aac809764
Sinan Keskin <sinan@kesk.in>
2022-02-13 14:24:52 +0300

    Move auto import to dependencies

--- index.js
+++ index.js
@@ -2,6 +2,16 @@
 
 module.exports = {
   name: require('./package').name,
+  options: {
+    autoImport: {
+      alias: {
+        'inputmask/dist/inputmask': 'inputmask/dist/inputmask',
+      },
+    },
+    babel: {
+      plugins: [require.resolve('ember-auto-import/babel-plugin')],
+    },
+  },
 
   included(app) {
     this._super.included.apply(this, arguments);
