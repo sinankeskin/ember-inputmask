4ff0e690764d07fc469b5617cbf2000eeeefe430
Marianne Bost <marianne.bost@pix.fr>
2023-10-10 16:43:56 +0200

    add tests

--- test-app/tests/integration/components/inputmask-test.js
+++ test-app/tests/integration/components/inputmask-test.js
@@ -11,46 +11,50 @@ module('Integration | Component | inputmask', function (hooks) {
   While there are not many tests, this verifys that inputmask was correctly attached to the input element.
   It also verifys that options, like mask and update are also passed correctly.
    */
-  test('component', async function (assert) {
-    this.value = undefined;
+  module('when the input is filled in', function () {
+    module('when update', function () {
+      test('it calls @update with value', async function (assert) {
+        this.value = undefined;
 
-    this.onUpdate = (value) => {
-      this.value = value;
-    };
+        this.onUpdate = (value) => {
+          this.value = value;
+        };
 
-    await render(
-      hbs`<Inputmask @mask='999 999 99 99' @update={{this.onUpdate}} />`
-    );
-    let inputMaskElement = find('input');
-    await fillIn(inputMaskElement, '98');
-    // verifying inputmask was correctly attached to inputmask and that the mask was passed
-    assert.dom(inputMaskElement).hasValue('98_ ___ __ __', 'text in input');
-    await blur(inputMaskElement);
-    // verifying the function was passed to inputmask
-    assert.strictEqual(this.value, '98', 'value');
-  });
+        await render(
+          hbs`<Inputmask @mask='999 999 99 99' @update={{this.onUpdate}} />`
+        );
 
-  /*
-  This does the same for the modifier
- */
-  test('modifier', async function (assert) {
-    this.value = undefined;
+        let inputMaskElement = find('input');
+        await fillIn(inputMaskElement, '98');
+        // verifying inputmask was correctly attached to inputmask and that the mask was passed
+        assert.dom(inputMaskElement).hasValue('98_ ___ __ __', 'text in input');
+        await blur(inputMaskElement);
+        // verifying the function was passed to inputmask
+        assert.strictEqual(this.value, '98', 'value');
+      });
+    });
 
-    this.onIncomplete = (event) => {
-      this.value = event.target.value;
-    };
+    module('when onIncomplete', function () {
+      test('it calls onIncomplete with event', async function (assert) {
+        this.value = undefined;
 
-    await render(
-      hbs`<input {{inputmask mask='999 999 99 99' onincomplete=this.onIncomplete}} />`
-    );
+        this.onIncomplete = (event) => {
+          this.value = event.target.value;
+        };
 
-    let inputMaskElement = find('input');
-    await fillIn(inputMaskElement, '98');
-    // verifying inputmask was correctly attached to inputmask and that the mask was passed
-    assert.dom(inputMaskElement).hasValue('98_ ___ __ __', 'text in input');
-    await blur(inputMaskElement);
-    // verifying the function was passed to inputmask
-    assert.strictEqual(this.value, '98_ ___ __ __', 'value');
+        await render(
+          hbs`<input {{inputmask mask='999 999 99 99' onincomplete=this.onIncomplete}} />`
+        );
+
+        let inputMaskElement = find('input');
+        await fillIn(inputMaskElement, '98');
+        // verifying inputmask was correctly attached to inputmask and that the mask was passed
+        assert.dom(inputMaskElement).hasValue('98_ ___ __ __', 'text in input');
+        await blur(inputMaskElement);
+        // verifying the function was passed to inputmask
+        assert.strictEqual(this.value, '98_ ___ __ __', 'value');
+      });
+    });
   });
 
   test('updates when params change', async function (assert) {
